172e586a5e92458d3872c5f22923f537347dcf85
Álvaro Castro Valverde <acastrova@usal.es>
2025-09-18 11:22:08 +0200

    Add files via upload

---
+++ Proyecto0/Proyecto0/CodeFile1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace Proyectos.ElSegundo
+{
+    class Basica
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Window win = new Window();
+            win.Title = "Segundo Programa";
+            win.Show();
+            Console.WriteLine("Ventana abierta");
+
+            Label etiqueta = new Label();
+            etiqueta.Content = "Hola Mundo";
+            win.Content = etiqueta;
+            Application app = new Application();
+            Console.WriteLine("Entrando en el bucle de mensajes");
+            app.Run();
+            Console.WriteLine("Acabamdo bucle");
+        }
+    }
+}
