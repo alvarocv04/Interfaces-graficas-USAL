9e08b68ca1a3477d9770209ae7cda75f8a2261ac
Álvaro Castro Valverde <acastrova@usal.es>
2025-10-16 12:55:14 +0200

    Add files via upload

---
+++ Practica3/Practica3/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Polyline polilinea;
+        Line ejeX;
+        Line ejeY;
+        public MainWindow()
+        {   
+            
+            InitializeComponent();
+            polilinea = new Polyline();
+            polilinea.Stroke = Brushes.Red;
+            lienzo.Children.Add(polilinea);
+            
+            ejeX = new Line();
+            ejeX.Stroke = Brushes.Black;
+            lienzo.Children.Add(ejeX);
+            
+            ejeY = new Line();
+            ejeY.Stroke = Brushes.Black;
+            lienzo.Children.Add(ejeY);
+        }
+
+        void dibuja()
+        {
+            double xreal, yreal, xrealmax = 10, xrealmin = -5, yrealmin = -10, yrealmax = 110;
+            double xpant, ypant, xpantmax, xpantmin, ypantmax, ypantmin;
+            xpantmin = lienzo.ActualWidth / 2;
+            ypantmin = 0;
+            xpantmax = lienzo.ActualWidth;
+            ypantmax = lienzo.ActualHeight / 2;
+            int numpuntos = (int)lienzo.ActualWidth;
+
+
+            polilinea.Points.Clear();
+            for (int i = 0; i < numpuntos; i++)
+            {
+                xreal = xrealmin + i * (xrealmax - xrealmin) / numpuntos;
+                yreal = xreal * xreal;
+
+                xpant = (xpantmax - xpantmin) * (xreal - xrealmin) / (xrealmax - xrealmin) + xpantmin;
+                ypant = (ypantmin - ypantmax) * (yreal - yrealmin) / (yrealmax - yrealmin) + ypantmax;
+                Point pt = new Point(xpant, ypant);
+                polilinea.Points.Add(pt);
+            }
+            ejeX.X1 = xpantmin;
+            ejeX.X2 = xpantmax;
+            ejeX.Y1 = (ypantmin - ypantmax) * (0 - yrealmin) / (yrealmax - yrealmin) + ypantmax;
+            ejeX.Y2 = ejeX.Y1;
+
+            ejeY.Y1 = ypantmin;
+            ejeY.Y2 = ypantmax;
+            ejeY.X1 = (xpantmax - xpantmin) * (0 - xrealmin) / (xrealmax - xrealmin) + xpantmin;
+            ejeY.X2 = ejeY.X1;
+
+        }
+
+        private void Boton_Click(object sender, RoutedEventArgs e)
+        {
+
+            dibuja();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            dibuja();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dibuja();
+        }
+    }
+}
---
+++ Practica3/Practica3v2/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Parabola_en_clase
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Polyline polilinea;      // Ya no declaramos aquí la polilínea y los ejes porque los hemos definido en XAML
+        // Line ejex, ejey;
+
+        double xrealmax, xrealmin, yrealmax, yrealmin;    // Ponemos estos campos en la clase porque los vamos a 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ejex.RenderTransform = polilinea.RenderTransform;
+            ejey.RenderTransform = polilinea.RenderTransform;
+
+            xrealmax = 20;
+            xrealmin = -15;
+            yrealmax = 410;
+            yrealmin = -10;
+            panel.Width = xrealmax - xrealmin;
+            panel.Height = yrealmax - yrealmin;
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            //   dibuja();
+        }
+
+        void dibuja()       // Solo trabajamos con coordenadas reales porque las coordenadas de pantalla las van a gestionar las transformaciones
+        {
+            int numpuntos = (int)panel.ActualWidth;  // nº de puntos de la polilínea
+            double xreal, yreal;
+
+            polilinea.Points.Clear();
+
+            for (int i = 0; i < numpuntos; i++)
+            {
+                xreal = xrealmin + i * (xrealmax - xrealmin) / numpuntos;
+                yreal = -xreal * xreal;                     // Multiplicamos la y por -1 para compensar el crecimiento inverso de las y en pantalla
+
+                Point pt = new Point(xreal, yreal);
+                polilinea.Points.Add(pt);
+            }
+
+            ejex.X1 = xrealmin;
+            ejex.Y1 = 0;
+            ejex.X2 = xrealmax;
+            ejex.Y2 = 0;
+
+            ejey.X1 = 0;
+            ejey.Y1 = -yrealmin;
+            ejey.X2 = 0;
+            ejey.Y2 = -yrealmax;
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Ya no tenemos que recalcular los puntos de la polilínea, solo tenemos que ajustar las propiedades de la transformación de traslación de la polilínea
+            // Del escalado se encarga el ViewBox
+
+            tt1.X = -xrealmin;
+            tt1.Y = yrealmax;
+
+            polilinea.StrokeThickness = 1 / Math.Max(miviewbox.ActualWidth / panel.Width, miviewbox.ActualHeight / panel.Height);
+            ejex.StrokeThickness = panel.Height / miviewbox.ActualHeight;
+            ejey.StrokeThickness = panel.Width / miviewbox.ActualWidth;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dibuja();
+        }
+    }
+}
