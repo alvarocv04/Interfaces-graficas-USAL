b14ab361be42e4a4c9da9f98ea6cc0a90ec8ef1a
Álvaro Castro Valverde <acastrova@usal.es>
2025-10-23 11:32:24 +0200

    Add files via upload

---
+++ Practica3/Practica3.2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Polyline polilinea;
+
+        public MainWindow()
+        {   
+            
+            InitializeComponent();
+            polilinea = new Polyline();
+            polilinea.Stroke = Brushes.Red;
+            lienzo.Children.Add(polilinea);
+            
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point punto = e.GetPosition(lienzo);
+
+            polilinea.Points.Add(punto);
+        }
+    }
+}
