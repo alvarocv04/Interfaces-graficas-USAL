e5ca42a1699b0ebfdff8dc85eaebab4bfaa0cebf
Álvaro Castro Valverde <acastrova@usal.es>
2025-10-30 13:02:03 +0100

    Add files via upload

---
+++ Sesion4/Sesion4/Amigo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion4
+{
+    internal class Amigo
+    {
+        public string nombre {  get; set; }
+        public string apellido { get; set; }
+        public  int edad {  get; set; }
+
+        public Amigo(String nombre, String apellido, int edad)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+
+        override public string ToString()
+        {
+            return nombre + " " + apellido + " Edad: " + edad;
+        }
+
+    }
+}
---
+++ Sesion4/Sesion4/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sesion4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Amigo> misamigos;
+        public MainWindow()
+        {
+            InitializeComponent();
+            misamigos= new List<Amigo>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Amigo amigo = new Amigo(caja1.Text, caja2.Text, 20); //La edad me la invento
+            misamigos.Add(amigo);
+            tabla.Items.Add(amigo);
+        }
+
+        private void tabla_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //No debo hacer nada con el elemento seleccionado sin previamente comprobar que es distinto de null
+            if(tabla.SelectedItem != null)
+            {
+                Amigo amigo = (Amigo)tabla.SelectedItem;
+                amigo.edad += 2;
+                etiq.Content = amigo;
+                tabla.Items.Refresh();
+            }
+            /*if (tabla.SelectedIndex != -1)
+            {
+                string s = (String)tabla.Items[tabla.SelectedIndex];
+                etiq.Content = s
+            }*/
+            
+        }
+    }
+}
