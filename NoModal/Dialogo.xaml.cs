45d80d6c5ee52cb07768fff995239284b762a4d9
Álvaro Castro Valverde <acastrova@usal.es>
2025-11-06 13:00:41 +0100

    Add files via upload

---
+++ NoModal/Dialogo.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sesion5
+{
+    /// <summary>
+    /// Lógica de interacción para Dialogo.xaml
+    /// </summary>
+    public class CadenaEventArgs : EventArgs
+    {
+        public string Cadena { get; set; }
+        public CadenaEventArgs(string s) 
+        { Cadena = s; }
+    }
+    
+    public partial class Dialogo : Window
+    {
+        public event EventHandler<CadenaEventArgs> HayNuevaCadena;  //NO USAR ACTION
+        public string cadena
+        { 
+            get { 
+                return cajatexto.Text; 
+            }
+            set
+            {
+                cajatexto.Text = value;
+            }
+        }
+        void OnHayNuevaCadena(string s)
+        {
+            if (HayNuevaCadena != null) {
+                HayNuevaCadena(this, new CadenaEventArgs(s));
+            }
+        }
+        public Dialogo()
+        {
+            InitializeComponent();
+        }
+
+        private void cerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void cajatexto_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnHayNuevaCadena(cadena);
+        }
+    }
+}
---
+++ NoModal/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sesion5
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dialogo dialogo = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (dialogo == null)
+            {
+                dialogo= new Dialogo();
+                dialogo.Owner = this; //Es para no poder poner por delante la otra ventana
+                dialogo.HayNuevaCadena += Dialogo_HayNuevaCadena;
+                dialogo.Closed += Dialogo_Closed;
+            }
+            dialogo.cadena = Title;
+            dialogo.Show(); //Modo no modal
+            
+            
+        }
+
+        private void Dialogo_Closed(object sender, EventArgs e)
+        {
+            dialogo = null;
+        }
+
+        private void Dialogo_HayNuevaCadena(object sender, CadenaEventArgs e)
+        {
+            Title = e.Cadena;
+        }
+    }
+}
